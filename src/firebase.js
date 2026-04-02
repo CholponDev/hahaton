e06a47d34cc3697260f4ed5e63fe97aeac2b53ce
Cholpon <januzakova.cholpon@gmail.com>
2026-04-02 07:18:04 +0600

    Отредоктировала Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
-export const db = getFirestore(app);
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAvSEvQIyCMSbY2T0zySP9Gu2pEDkSbvF8",
   authDomain: "book-store-832c7.firebaseapp.com",
@@ -20,6 +14,6 @@ const firebaseConfig = {
   measurementId: "G-223X2FXLJ6"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 const analytics = getAnalytics(app);
